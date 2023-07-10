4f3c36f62c56f34ec206ec7223bb7fa5b5a6ac54
rector6 <peteroche32@gmail.com>
2023-07-09 23:02:40 -0700

    update

--- public/script.js
+++ public/script.js
@@ -74,3 +74,29 @@ const tabs = document.querySelectorAll('[data-target]'),
             tab.classList.add('qualification_active')
         })
     })
+
+
+    // ============social modal
+
+    const modalViews = document.querySelectorAll('.services_modal'),
+          modalBtns = document.querySelectorAll('.services_button'),
+          modalCloses = document.querySelectorAll('.services_modal-close')
+
+
+    let modal = function(modalClick) {
+        modalViews[modalClick].classList.add('active-modal')
+    }
+
+    modalBtns.forEach((modalBtn, i) =>{
+        modalBtn.addEventListener('click', () =>{
+            modal(i)
+        })
+    })
+
+    modalCloses.forEach((modalClose) =>{
+        modalClose.addEventListener('click', () => {
+            modalViews.forEach((modalView) =>{
+                modalView.classList.remove('active-modal')
+            })
+        })
+    })
